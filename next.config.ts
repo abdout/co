2ea023687a5ed1524ce9bd4ea36fd546f879385b
abdout <osmanabdout@hotmail.com>
2025-03-30 21:52:54 +0300

    block

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  assetPrefix: "/blocks/_next/", // Prefix all asset URLs
   eslint: {
     ignoreDuringBuilds: true,
   },
