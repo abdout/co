1d46787d6722df1a361641e88d2c79ceef2f7793
abdout <osmanabdout@hotmail.com>
2025-03-30 21:11:59 +0300

    g

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: "/blocks/_next/", // Prefix all asset URLs
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
-  // basePath and assetPrefix removed as per recommendations
+  },
 };
 
 export default nextConfig;
