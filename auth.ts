4f957c40ccb8d07d75d282e820c71420234ce3b1
abdout <osmanabdout@hotmail.com>
2025-04-10 22:20:51 +0300

    auth

--- auth.ts
+++ auth.ts
@@ -90,7 +90,7 @@ export const {
       const existingAccount = await getAccountByUserId(existingUser.id)
 
       token.isOAuth = !!existingAccount
-      token.name = existingUser.name
+      token.name = existingUser.username
       token.email = existingUser.email
       token.role = existingUser.role
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
